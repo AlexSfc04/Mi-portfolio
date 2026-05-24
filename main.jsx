6c67d59be3087a51d5dbc621396510317c47e42c
AlexSfc04 <amoricoalejandro@alumnos.cei.es>
2026-05-24 18:24:33 +0200

    responsive

--- main.jsx
+++ main.jsx
@@ -9,3 +9,11 @@ createRoot(document.getElementById("root")).render(
     <App />
   </StrictMode>
 );
+
+// Hide drawer element
+setTimeout(() => {
+  const drawer = document.querySelector('._drawer_1w1y8_123');
+  if (drawer) {
+    drawer.style.display = 'none';
+  }
+}, 0);
